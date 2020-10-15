4058caba39a8510e3a2f91c19cdec44f69a1a190
DineshRout779 <rddinesh779@gmail.com>
2020-10-15 10:48:12 +0530

    click outside to close nav

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-$(function(){
-    $(".menu").click(function(){
-        $(".links").toggleClass("open");
-        $(".bar").toggleClass("open");
+$(document).ready(function () {
+    $(".menu").click(function (e) { 
+        e.preventDefault();
+        $(".links").toggleClass("open");  // Close Nav when clicked outside
     });
-})
-
+    $(document).click(function (e) { 
+        if(!$(".links").is(e.target) && !$(".menu").is(e.target)){
+            if($('.links').hasClass("open")){
+                $(".links").removeClass("open");
+            }
+        }
+    });
+});
 
